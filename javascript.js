0923587338087b4d7f80228010e4d8e7aabfd03b
Hermloth <herm_loth@hotmail.com>
2024-06-05 02:10:46 +1000

    Add color on mouseover

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 
 //Global Variables
 var canvasSize = 16;
+var cellItteration = 0;
 const maxCanvasSize = 100;
 const changeCanvasButton = document.querySelector("#ChangeCanvas");
 const resetCanvasButton = document.querySelector("#Reset")
@@ -8,10 +9,27 @@ const canvasArea = document.querySelector("#playArea");
 
 changeCanvasButton.addEventListener("click", function () {
     canvasSize = prompt ("Please enter a canvas size between 1 and 100.");
-    SelectCanvasSize(canvasSize);  
-});
+    var canvasSizeInt = parseInt(canvasSize);
+    if (isNaN(canvasSizeInt)) {
+        ThrowInputError();
+    } else {
+        if (typeof canvasSizeInt === "number"){
+            SelectCanvasSize(canvasSizeInt); 
+        } else {
+            ThrowInputError();
+        }
+    }  
+    }
+);
 
 resetCanvasButton.addEventListener("click", ClearCanvas);
+
+function ThrowInputError(){
+    ClearCanvas();
+    var errorMessage = document.createElement("p");
+    errorMessage.textContent = "Invalid Selection, please select an integer size less than 100 squares.";
+    canvasArea.appendChild(errorMessage);
+}
 
 function mouseHover (){
  //TODO: Implement hover listener
@@ -32,31 +50,38 @@ function CreateGridCell(iteration){
     for (cell=0; cell<canvasSize; cell++){
         var newGridCell = document.createElement("div");
         newGridCell.classList.add("gridCell");
+        newGridCell.setAttribute("id", cellItteration)
+        newGridCell.addEventListener("mouseover", function(event){
+            console.log("mouseover detected");
+            this.style.backgroundColor = "red";
+        })
         gridRow.appendChild(newGridCell);
     }
 }
 
 
 function DrawCanvas(inputSize){
-// TODO: Call Canvas Setup without a new input
-console.log("Size selection is " + inputSize);
-for (row=0; row<inputSize; row++){
-    SetUpGridRow(row);
-}
-// -----------------------------
+    // Create a new canvas
+    console.log("Size selection is " + inputSize);
+    for (row=0; row<inputSize; row++){
+        SetUpGridRow(row);
+    }
 }
 
-function ClearCanvas(){ //Clear the canvas for a new selection
+function ClearCanvas(){
+    //Clear the canvas for a new selection        
+    var errorMessage = document.createElement("p");
+    errorMessage.textContent = "Invalid Selection, please select an integer size less than 100 squares.";
+    canvasArea.appendChild(errorMessage);
     canvasArea.replaceChildren();
+    cellItteration = 0;
 }
 
 function SelectCanvasSize(canvasInput){
-    // TODO: Handle String Input Error
+    // Generate a new user specified canvas and catch invalid inputs
     ClearCanvas();
-    if (canvasInput > 100 | canvasInput < 1 | canvasInput != typeof(Int)) {
-        var errorMessage = document.createElement("p");
-        errorMessage.textContent = "Invalid Selection, please select an integer size less than 100 squares.";
-        canvasArea.appendChild(errorMessage);
+    if (canvasInput > 100 | canvasInput < 1) {
+        ThrowInputError();
     } else {
         var testMessage = document.createElement("p");
         testMessage.textContent = "Test Message for less than 100 grid squares";
