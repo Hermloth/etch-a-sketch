132e1fcbf1dfdb66d1f161bdf9ed14a1ade344a7
Hermloth <herm_loth@hotmail.com>
2024-06-04 22:52:14 +1000

    setup initial function

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+//Global Variables
+var canvasSize = 16;
+const maxCanvasSize = 100;
+const changeCanvasButton = document.querySelector("#ChangeCanvas");
+const resetCanvasButton = document.querySelector("#Reset")
+
+changeCanvasButton.addEventListener();
+
+function mouseHover (){
+ //TODO: Implement hover listener
+}
+
+
+function drawCanvas(inputSize){
+// TODO: Call Canvas Setup without a new input
+console.log(inputSize);
+// -----------------------------
+}
+
+function selectCanvasSize(){
+    var canvasInput = prompt ("Please enter a canvas size.");
+    if (canvasInput > 100) {
+        //TODO: return user to web page and show too large error
+    } else {
+        //TODO: Call Canvas Setup 
+    }
+}
+
+
+
+
+drawCanvas(canvasSize);
+
+
+
