452836b999dc4d96f59855fb40d3f7e7458d9b67
Hermloth <herm_loth@hotmail.com>
2024-06-05 01:09:23 +1000

    Setup Grid and reset logic

--- javascript.js
+++ javascript.js
@@ -4,33 +4,68 @@ var canvasSize = 16;
 const maxCanvasSize = 100;
 const changeCanvasButton = document.querySelector("#ChangeCanvas");
 const resetCanvasButton = document.querySelector("#Reset")
+const canvasArea = document.querySelector("#playArea");
 
-changeCanvasButton.addEventListener();
+changeCanvasButton.addEventListener("click", function () {
+    canvasSize = prompt ("Please enter a canvas size between 1 and 100.");
+    SelectCanvasSize(canvasSize);  
+});
+
+resetCanvasButton.addEventListener("click", ClearCanvas);
 
 function mouseHover (){
  //TODO: Implement hover listener
 }
 
-
-function drawCanvas(inputSize){
-// TODO: Call Canvas Setup without a new input
-console.log(inputSize);
-// -----------------------------
+function SetUpGridRow(rowIteration) {
+    // Create new grid rows
+    var newGridRow = document.createElement("div");
+    newGridRow.classList.add("gridRow");
+    canvasArea.appendChild(newGridRow);
+    CreateGridCell(rowIteration);
+    console.log("row itteration");
 }
 
-function selectCanvasSize(){
-    var canvasInput = prompt ("Please enter a canvas size.");
-    if (canvasInput > 100) {
-        //TODO: return user to web page and show too large error
-    } else {
-        //TODO: Call Canvas Setup 
+function CreateGridCell(iteration){
+    // Create new grid cells
+    var gridRow = document.getElementsByClassName("gridRow")[iteration];
+    for (cell=0; cell<canvasSize; cell++){
+        var newGridCell = document.createElement("div");
+        newGridCell.classList.add("gridCell");
+        gridRow.appendChild(newGridCell);
     }
 }
 
 
+function DrawCanvas(inputSize){
+// TODO: Call Canvas Setup without a new input
+console.log("Size selection is " + inputSize);
+for (row=0; row<inputSize; row++){
+    SetUpGridRow(row);
+}
+// -----------------------------
+}
+
+function ClearCanvas(){ //Clear the canvas for a new selection
+    canvasArea.replaceChildren();
+}
+
+function SelectCanvasSize(canvasInput){
+    // TODO: Handle String Input Error
+    ClearCanvas();
+    if (canvasInput > 100 | canvasInput < 1 | canvasInput != typeof(Int)) {
+        var errorMessage = document.createElement("p");
+        errorMessage.textContent = "Invalid Selection, please select an integer size less than 100 squares.";
+        canvasArea.appendChild(errorMessage);
+    } else {
+        var testMessage = document.createElement("p");
+        testMessage.textContent = "Test Message for less than 100 grid squares";
+        DrawCanvas(canvasInput);
+    }
+}
 
 
-drawCanvas(canvasSize);
+DrawCanvas(canvasSize);
 
 
 
